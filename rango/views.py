9a08fdd145d7baa3dfdccb229a926e7622e82958
Andreas Jacobsen <andreas1300@gmail.com>
2017-06-04 19:45:02 +0200

    Lagt til en statics mappe og en html-fil.

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hi!")
+
+    # Construct a citionary to pass to the template engine as it's contect.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "Crunch, creamy, cookie, candy, cupcake!"}
+    return render (request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='/rango'> Back to index</a>")
